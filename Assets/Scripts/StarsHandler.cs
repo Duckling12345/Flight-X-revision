6de486e20723a97c1fe5f708547749a495a5043b
yantans <129519076+yantans@users.noreply.github.com>
2024-04-11 20:37:52 +0800

    added level result test on demo and created a script

--- Assets/Scripts/StarsHandler.cs
+++ Assets/Scripts/StarsHandler.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class StarsHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] stars;
+    private int objCount;
+
+    private void Start()
     {
-        
+        objCount = GameObject.FindGameObjectsWithTag("Objective").Length;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void starsAchieved()
     {
-        
+        int objsLeft = GameObject.FindGameObjectsWithTag("Objective").Length;
+        int objsCompleted = objCount - objsLeft;
+
+        float percentage = float.Parse(objsCompleted.ToString()) / float.Parse(objCount.ToString()) * 100f;
+
+        if (percentage >= 33f && percentage < 66)
+        {
+            //one star
+            stars[0].SetActive(true);
+        }
+        else if (percentage >= 666 && percentage < 70)
+        {
+            //two stars
+            stars[0].SetActive(true);
+            stars[1].SetActive(true);
+        }
+        else
+        {
+            //three stars
+            stars[0].SetActive(true);
+            stars[1].SetActive(true);
+            stars[2].SetActive(true);
+        }
     }
 }
+
