873c22c516ba8d1930bcc58ca82f8792f3e49014
Sniping_Duckling <raineeecarlooo23@gmail.com>
2024-05-17 00:54:06 +0800

    update
    adjustment score

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -23,6 +23,12 @@ public class ScoreUI : MonoBehaviour
         int lopscore = PlayerPrefs.GetInt("lopScore", 0);
         int fobscore = PlayerPrefs.GetInt("fobScore", 0);
         int wlscore =  PlayerPrefs.GetInt("wlScore", 0);
+        int lopTime = PlayerPrefs.GetInt("lopTime", 0);
+        int fobTime = PlayerPrefs.GetInt("fobTime", 0);
+        int waterTime = PlayerPrefs.GetInt("waterTime", 0);
+
+
+
 
         //Rows
         var row1 = Instantiate(rowUi, transform).GetComponent<RowUI>();
@@ -30,23 +36,29 @@ public class ScoreUI : MonoBehaviour
         row1.levelname.text = "Pre-Flight Checking"; 
         //lagay time left
         row1.score.text = htpscore.ToString() + "/" + totalQuestions;
+        row1.timeleft.text = "N/A";
+
+
 
         var row2 = Instantiate(rowUi, transform).GetComponent<RowUI>();
         row2.rank.text = "2";
         row2.levelname.text = "Loss of Pressurization";
         //lagay time left
         row2.score.text = lopscore.ToString() + "/" + totalQuestions;
+        row2.timeleft.text = lopTime.ToString() + " seconds";
 
         var row3 = Instantiate(rowUi, transform).GetComponent<RowUI>();
         row3.rank.text = "3";
         row3.levelname.text = "Fire Onboard";
         //lagay time left
         row3.score.text = fobscore.ToString() + "/" + totalQuestions;
+        row3.timeleft.text = fobTime.ToString() + " seconds";
 
         var row4 = Instantiate(rowUi, transform).GetComponent<RowUI>();
         row4.rank.text = "4";
         row4.levelname.text = "Water Landing";
         //lagay time left
         row4.score.text = wlscore.ToString() + "/" + totalQuestions;
+        row4.timeleft.text = waterTime.ToString() + " seconds";
     }
 }
